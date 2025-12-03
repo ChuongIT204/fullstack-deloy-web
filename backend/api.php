2838def9d3a0439b6f0710a71a0c1163a2e6e36e
nguyenthe <thechuong.0101@gmail.com>
2025-12-03 07:55:43 +0700

    update

--- backend/api.php
+++ backend/api.php
@@ -3,6 +3,7 @@ header('Content-Type: application/json');
 
 
 header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Credentials: true');
 header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type, Authorization');
 
